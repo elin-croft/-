93e90f82ccb7cf109fc5da5006394a3687704089
elin-croft <1060985887@qq.com>
2019-11-01 21:28:19 +0800

    ex 1.3-1.6

--- C++/day1/test.cpp
+++ C++/day1/test.cpp
@@ -1,12 +1,15 @@
 #include<iostream>
+#include"Sales_item.hpp"
 int main()
 {
-    int val=0,ans=0;
-    while(std::cin>>val)
-    {
-        ans += val;
-    }
-    std::cout<<ans<<std::endl;
-    system("pause");
+    int v1, v2;
+    std::cout<<"hello world"<<std::endl;
+    std::cin>>v1>>v2;
+    std::cout<<"the sum of "<<v1
+            <<" and "<<v2<<" is "<<v1+v2<<std::endl;
+    std::cout<<"the multiplication of "<<v1
+            <<" and "<<v2<<" is "<<v1*v2<<std::endl;
+    std::cout<<"and of course the program in ex1.6 is illegal"<<'\n'<<
+                "to correct it you need to erase the ';' after v1 and v2 "<<std::endl;
     return 0;
 }
