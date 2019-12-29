0fa81da4c0837b338d9a258e6072c58770f03213
elin-croft <wangyilin19941@outlook.com>
2019-12-29 23:47:37 +0800

    update

--- C++/part1/chapter3/es3.5.3.cpp
+++ C++/part1/chapter3/es3.5.3.cpp
@@ -12,17 +12,15 @@ using std::vector;
 
 int main()
 {
-    int a[9] = {0},b[9]={0};
+    int a[9] = {1},b[9]={1};
     int flag = 0;
-    for(int i = 0; i < 9; i++)
-    {
-        cin>>a[i];
-        cin>>b[i];
-        cout<<endl<<i<<endl;
-    }
+    a[8] = 2;
+    b[8] = 9;
     int *p1 = a, *p2 = b;
     if(getsize(a, int) != getsize(b, int))
         flag = 1;
+    vector<int>av(std::begin(a), std::end(a));
+    int *ca=nullptr;
     while(p1 != std::end(a) && p2 != std::end(b) && flag != 1)
     {
         if(*p1 != *p2)
@@ -30,9 +28,20 @@ int main()
         p1++;
         p2++;
     }
+    for(auto tmp = av.begin(); tmp != av.end(); tmp++)
+        cout<<*tmp<<" ";
+    ca = new int[av.size()];
+    for(int i = 0; i < getsize(a, int); i++)
+    {
+        *(ca + i) = b[i];
+    }
+    cout<<endl;
+    for(auto tmp = ca; tmp != ca + 9; tmp++)
+        cout<<*tmp<<" ";
     if(flag == 1)
         cout<<"array a and array b is not the same"<<endl;
     else
         cout<<"two arrays are the same"<<endl;
+    delete [] ca;
     return 0;
 }
